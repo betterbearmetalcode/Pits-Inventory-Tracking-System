22f0cbba253ad63f2464f4d01a7ee1ea7be6b349
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2024-02-20 18:53:05 -0800

    create GridPane-based view for import process

---
+++ src/main/java/org/bearmetal/pits_inventory_tracking_system/controllers/ImportController.java
@@ -0,0 +1,10 @@
+package org.bearmetal.pits_inventory_tracking_system.controllers;
+
+import org.bearmetal.pits_inventory_tracking_system.utils.PageLoader;
+
+public class ImportController {
+    PageLoader pageLoader = new PageLoader();
+
+    
+}
+

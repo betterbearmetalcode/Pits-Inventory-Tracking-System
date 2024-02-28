3370319a1fefbc0d4753ce60b47c398c79a1f84c
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2024-02-27 22:20:46 -0800

    Rename DataImporter to ImportBackend

--- src/main/java/org/bearmetal/pits_inventory_tracking_system/ImportBackend.java
+++ src/main/java/org/bearmetal/pits_inventory_tracking_system/ImportBackend.java
@@ -10,58 +10,60 @@ import java.util.Scanner;
 
 import org.bearmetal.pits_inventory_tracking_system.utils.DatabaseManager;
 
-/** Handles initial data import.
+/**
+ * Handles initial data import.
  * 
  * @author Colin Rice
  */
-public class DataImporter {
+public class ImportBackend {
 
     private static Random rand = new Random();
 
-    //Map of category name to category ID
+    // Map of category name to category ID
     private static HashMap<String, Integer> categoryMapping = new HashMap<String, Integer>();
     private static ArrayList<Integer> locationIDWorkingSet = new ArrayList<Integer>();
     private static ArrayList<Integer> itemIDWorkingSet = new ArrayList<Integer>();
     private static Integer currentLocation;
 
-    private static Integer generateID(){
+    private static Integer generateID() {
         Integer id = rand.nextInt(100000000, 999999999);
-        //If either of our two working sets contains our ID, skip it.
-        //Hopefully this doesn't cause too much recursion?
-        if (locationIDWorkingSet.contains(id) || itemIDWorkingSet.contains(id)){
+        // If either of our two working sets contains our ID, skip it.
+        // Hopefully this doesn't cause too much recursion?
+        if (locationIDWorkingSet.contains(id) || itemIDWorkingSet.contains(id)) {
             return generateID();
         }
         return id;
     }
 
-    private static void addLocation(Integer locationID, String locationName, Integer isCategory, Integer parentCategory) throws SQLException{
-        Object[] params = {locationID, locationName, isCategory, parentCategory};
+    private static void addLocation(Integer locationID, String locationName, Integer isCategory, Integer parentCategory)
+            throws SQLException {
+        Object[] params = { locationID, locationName, isCategory, parentCategory };
         locationIDWorkingSet.add(locationID);
         DatabaseManager.execNoReturn("INSERT INTO locations VALUES(?, ?, ?, ?)", params);
         System.out.println("Added location " + locationID);
-        //Probably shouldn't do this but I think it'll work anyways.
+        // Probably shouldn't do this but I think it'll work anyways.
         currentLocation = locationID;
     }
 
-    private static void processLocation(String[] curDataSet) throws SQLException{
+    private static void processLocation(String[] curDataSet) throws SQLException {
         String locationName = curDataSet[0].strip();
-        //Is this location part of a category?
-        if (locationName.contains("-")){
+        // Is this location part of a category?
+        if (locationName.contains("-")) {
             String[] categoryPair = locationName.split("-", 2);
             String parentName = categoryPair[0].strip();
             String childName = categoryPair[1].strip();
             Integer parentID;
             System.out.println("Parent category: " + parentName);
             System.out.println("Child category: " + childName);
-            //Do we have a parent category already registered?
-            if (! categoryMapping.containsKey(parentName)){
-                //No? Add it.
+            // Do we have a parent category already registered?
+            if (!categoryMapping.containsKey(parentName)) {
+                // No? Add it.
                 parentID = generateID();
                 addLocation(parentID, parentName, 1, 0);
             } else {
                 parentID = categoryMapping.get(parentName);
             }
-            //Now add this location.
+            // Now add this location.
             Integer childID = generateID();
             addLocation(childID, childName, 0, parentID);
         } else {
@@ -70,9 +72,9 @@ public class DataImporter {
         }
     }
 
-    private static Integer getInteger(String in){
+    private static Integer getInteger(String in) {
         in = in.strip().replace("\"", "");
-        if (in.equals("") || in.equals("FALSE")){
+        if (in.equals("") || in.equals("FALSE")) {
             return 0;
         } else if (in.equals("TRUE")) {
             return 1;
@@ -80,7 +82,7 @@ public class DataImporter {
         return Integer.parseInt(in);
     }
 
-    private static void processItem(String[] curDataSet) throws SQLException{
+    private static void processItem(String[] curDataSet) throws SQLException {
         Integer itemID = generateID();
         String itemName = curDataSet[1];
         String itemDescription = curDataSet[2];
@@ -92,48 +94,52 @@ public class DataImporter {
         Integer packed = getInteger(curDataSet[7]);
         Integer locationID = currentLocation;
         System.out.println("Added item " + itemID);
-        Object[] params = {itemID, itemName, itemDescription, itemQuantity, itemAvailable, itemVendor, partNumber, itemInfo, packed, locationID, ""};
+        Object[] params = { itemID, itemName, itemDescription, itemQuantity, itemAvailable, itemVendor, partNumber,
+                itemInfo, packed, locationID, "" };
         DatabaseManager.execNoReturn("INSERT INTO items VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", params);
     }
 
-    private static void parseLine(String line) throws SQLException{
-        //Break the current line into individual columns.
-        //This regex matches all commas EXCEPT commas in string literals.
-        //Any commas in item names/descriptions would fuck us over otherwise 
-        //Limit is set to -1 to allow empty strings so we can index columns properly even if one or more is empty.
+    private static void parseLine(String line) throws SQLException {
+        // Break the current line into individual columns.
+        // This regex matches all commas EXCEPT commas in string literals.
+        // Any commas in item names/descriptions would fuck us over otherwise
+        // Limit is set to -1 to allow empty strings so we can index columns properly
+        // even if one or more is empty.
         String[] intermediate = line.split(",(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
-        if (intermediate[0].equals("") && intermediate[1].equals("") && intermediate[2].equals("")){
+        if (intermediate[0].equals("") && intermediate[1].equals("") && intermediate[2].equals("")) {
             System.out.println("Empty row, skipping.");
             return;
         }
-        if (intermediate[0] != ""){
-            if (intermediate[0].equals("Location")){
+        if (intermediate[0] != "") {
+            if (intermediate[0].equals("Location")) {
                 System.out.println("Skipping column headers.");
                 return;
             }
             processLocation(intermediate);
         } else {
-            if (currentLocation.equals(null)){
+            if (currentLocation.equals(null)) {
                 System.out.println("WARNING: Item before first location, ignoring");
                 return;
             }
-            try{
+            try {
                 processItem(intermediate);
-            } catch (NumberFormatException err){
-                System.out.println("ERROR: Invalid item quantity / part number / packed state. These values MUST be integers.");
+            } catch (NumberFormatException err) {
+                System.out.println(
+                        "ERROR: Invalid item quantity / part number / packed state. These values MUST be integers.");
                 System.out.println("Skipping this item.");
             }
         }
     }
 
-    /** Attempts to import data from a CSV file.
+    /**
+     * Attempts to import data from a CSV file.
      *
      * @param inputFile
-     * The file to import data from.   
+     *                  The file to import data from.
      */
-    public static void importFromFile(File inputFile) throws SQLException, FileNotFoundException{
-        try (Scanner inputFileScanner = new Scanner(inputFile)){
-            while (inputFileScanner.hasNextLine()){
+    public static void importFromFile(File inputFile) throws SQLException, FileNotFoundException {
+        try (Scanner inputFileScanner = new Scanner(inputFile)) {
+            while (inputFileScanner.hasNextLine()) {
                 parseLine(inputFileScanner.nextLine());
             }
         }
--- src/main/java/org/bearmetal/pits_inventory_tracking_system/MainApplication.java
+++ src/main/java/org/bearmetal/pits_inventory_tracking_system/MainApplication.java
@@ -18,27 +18,31 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException, SQLException {
         Thread.setDefaultUncaughtExceptionHandler(ErrorHandler::showFatalErrorDialog);
-        try{
+        try {
             HashMap<String, String> tables = new HashMap<String, String>();
-            tables.put("locations", "location_id INTEGER PRIMARY KEY, location_name TEXT, is_category INTEGER, parent_category INTEGER");
-            tables.put("items", "item_id INTEGER PRIMARY KEY, item_name TEXT, item_description TEXT, item_quantity INTEGER, item_available INTEGER, item_vendor TEXT, item_part_number TEXT, item_info TEXT, packed INTEGER, location_id INTEGER, photo_path TEXT");
-            tables.put("events", "item_id INTEGER, event_id INTEGER PRIMARY KEY, event_type INTEGER, event_bulk INTEGER, event_timestamp INTEGER, event_desc TEXT, item_delta INTEGER, item_state INTEGER");
+            tables.put("locations",
+                    "location_id INTEGER PRIMARY KEY, location_name TEXT, is_category INTEGER, parent_category INTEGER");
+            tables.put("items",
+                    "item_id INTEGER PRIMARY KEY, item_name TEXT, item_description TEXT, item_quantity INTEGER, item_available INTEGER, item_vendor TEXT, item_part_number TEXT, item_info TEXT, packed INTEGER, location_id INTEGER, photo_path TEXT");
+            tables.put("events",
+                    "item_id INTEGER, event_id INTEGER PRIMARY KEY, event_type INTEGER, event_bulk INTEGER, event_timestamp INTEGER, event_desc TEXT, item_delta INTEGER, item_state INTEGER");
             DatabaseManager.setTables(tables);
             ApplicationSetup.doInitialSetup();
             FXMLLoader initialSceneLoader;
             ApplicationSetup.doDatabaseSetup();
-            DataImporter.importFromFile(new File("testData.csv"));
-            if (ApplicationVars.isFirstRun){
+            ImportBackend.importFromFile(new File("testData.csv"));
+            if (ApplicationVars.isFirstRun) {
                 initialSceneLoader = new FXMLLoader(MainApplication.class.getResource("import.fxml"));
             } else {
                 initialSceneLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
             }
-            Scene scene = new Scene(initialSceneLoader.load(), ApplicationVars.windowWidth, ApplicationVars.windowHeight);
+            Scene scene = new Scene(initialSceneLoader.load(), ApplicationVars.windowWidth,
+                    ApplicationVars.windowHeight);
             stage.setTitle("2046 Pits Inventory Tracker System");
             stage.setScene(scene);
             stage.setResizable(true);
             stage.show();
-        } catch (Exception err){
+        } catch (Exception err) {
             ErrorHandler.showFatalErrorDialog(err);
         }
     }
